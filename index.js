1c510ad09bbf0312e0676d80f77cda2fdce4896d
Garvit Kataria <garvit.k16@iiits.in>
2018-09-21 19:36:02 +0530

    “8”

--- index.js
+++ index.js
@@ -23,11 +23,72 @@ restService.post("/echo", function(req, res) {
       ? req.body.queryResult.parameters.echoText
       : "Seems like some problem. Speak again.";
   console.log("speech",speech);
-  return res.json({
-    "speech": speech,
-    "displayText": speech,
-    "source": "webhook-echo-sample"
-  });
+  return res.json(
+  // {
+  //   "speech": speech,
+  //   "displayText": speech,
+  //   "source": "webhook-echo-sample",
+  //   "fulfillmentText": speech,
+
+  // }
+
+  {
+  "fulfillmentText": "This is a text response",
+  "fulfillmentMessages": [
+    {
+      "card": {
+        "title": "card title",
+        "subtitle": "card text",
+        "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+        "buttons": [
+          {
+            "text": "button text",
+            "postback": "https://assistant.google.com/"
+          }
+        ]
+      }
+    }
+  ],
+  "source": "example.com",
+  "payload": {
+    "google": {
+      "expectUserResponse": true,
+      "richResponse": {
+        "items": [
+          {
+            "simpleResponse": {
+              "textToSpeech": "this is a simple response"
+            }
+          }
+        ]
+      }
+    },
+    "facebook": {
+      "text": "Hello, Facebook!"
+    },
+    "slack": {
+      "text": "This is a text response for Slack."
+    }
+  },
+  "outputContexts": [
+    {
+      "name": "projects/contexts/context name",
+      "lifespanCount": 5,
+      "parameters": {
+        "param": "param value"
+      }
+    }
+  ],
+  "followupEventInput": {
+    "name": "event name",
+    "languageCode": "en-US",
+    "parameters": {
+      "param": "param value"
+    }
+  }
+}
+
+  );
 });
 
 
