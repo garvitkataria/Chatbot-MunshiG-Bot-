441932c4107c541f3cded15591665944a6571d29
Garvit Kataria <garvit.k16@iiits.in>
2018-09-21 19:18:30 +0530

    “4”

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
+  console.log(req.body);
   var speech =
     req.body.queryResult &&
     req.body.queryResult.parameters &&
